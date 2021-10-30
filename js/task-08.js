6a8d86ea0bfcec237c7ddc72734b6c43ef635629
LenaCScheper <ven.lenysja@ukr.net>
2021-10-30 14:54:34 +0300

    сделала 8 и 9 задание

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,14 @@
-const form = document.querySelector('.js/task-08.js');
+const form = document.querySelector('.login-form');
+
+form.login-form.addEventListener('submit', onFormSubmit);
+
+
+function onFormSubmit(event) {
+if(val.length>5){
+    alert("Недопустимая длина строки");
+    document.search.reset();
+    event.preventDefault();
+}   
+else
+    alert("Отправка разрешена");
+}
